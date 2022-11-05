7a26870e11bc5f8fcdfb94a08ab2c13d1805bcac
Andrii Senkovych <andrii_senkovych@epam.com>
2022-11-05 13:34:31 +0200

    Add Load Balancer resources

--- main.tf
+++ main.tf
@@ -125,3 +125,39 @@ resource "aws_lb" "app" {
   ]
   subnets = module.vpc.public_subnets
 }
+
+
+resource "aws_lb_target_group" "app" {
+  name     = "app"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 10
+    timeout             = 5
+    interval            = 10
+    path                = "/"
+    port                = "80"
+  }
+}
+
+
+resource "aws_lb_target_group_attachment" "app" {
+  count            = length(aws_instance.app)
+  target_group_arn = aws_lb_target_group.app.arn
+  target_id        = aws_instance.app[count.index].id
+}
+
+
+resource "aws_lb_listener" "app-http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "time_last_deployed" {
   value = timestamp()
 }
+
+output "url" {
+  value = "http://${aws_lb.app.dns_name}"
+}
