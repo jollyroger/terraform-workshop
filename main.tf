b5d2515eb4fe50d668b01f515d690046115edad2
Andrii Senkovych <andrii_senkovych@epam.com>
2022-11-05 12:06:15 +0200

    Add VPC

--- main.tf
+++ main.tf
@@ -5,9 +5,32 @@
 terraform {
   required_version = ">= 1.5"
   backend "s3" {}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 
 provider "aws" {
   region = var.region
+}
+
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "eMASE"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_ipv6        = false
 }
